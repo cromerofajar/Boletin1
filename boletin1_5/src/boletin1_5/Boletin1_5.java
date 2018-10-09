24fa91bf414e5747119e717022c9f95a71496f07
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2018-10-09 11:03:30 +0200

    Ejercicio1 inicio

---
+++ boletin1_5/src/boletin1_5/Boletin1_5.java
@@ -0,0 +1,18 @@
+
+package boletin1_5;
+
+import java.util.Scanner;
+
+public class Boletin1_5 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+        double millas, m=1852, metros;
+        System.out.println("introduzca millas");
+        millas= ler.nextDouble();
+        System.out.println(millas + " millas equivale a " + (millas*m)+" metros");
+        
+        // TODO code application logic here
+    }
+    
+}
