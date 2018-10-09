ba9681b2be1dc2d8de1c42bed54dc60733b73d18
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2018-10-09 11:01:42 +0200

    Ejercicio1 inicio

---
+++ boletin1_1/src/boletin1_1/Boletin1_1.java
@@ -0,0 +1,15 @@
+
+package boletin1_1;
+
+
+public class Boletin1_1 {
+
+    
+    public static void main(String[] args) {
+        float base=4f, altura=3f, area;
+        area= base*altura/2;
+        System.out.println("area=" + area);
+        // TODO code application logic here
+    }
+    
+}
